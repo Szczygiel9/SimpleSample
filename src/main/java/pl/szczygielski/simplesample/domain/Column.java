b79337cedb3808c533af4a0d325737b742ae5942
szczygiel9 <szczygielski.daniel38@gmail.com>
2019-09-06 12:45:24 +0200

    first domain documents

---
+++ src/main/java/pl/szczygielski/simplesample/domain/Column.java
@@ -0,0 +1,9 @@
+package pl.szczygielski.simplesample.domain;
+
+import lombok.Getter;
+
+@Getter
+public class Column {
+    private String name;
+    private ColumnType type;
+}
---
+++ src/main/java/pl/szczygielski/simplesample/domain/ColumnType.java
@@ -0,0 +1,5 @@
+package pl.szczygielski.simplesample.domain;
+
+public enum ColumnType {
+    VARCHAR, BOOL, INTEGER, FLOAT, DATE;
+}
