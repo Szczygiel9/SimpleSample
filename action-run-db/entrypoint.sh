bfda7c4599eb08c602840a44502269e561ff708e
szczygiel9 <szczygielski.daniel38@gmail.com>
2019-09-06 14:21:21 +0200

    removed actions folder, added sample test class, changed CI workflow

--- action-run-db/entrypoint.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh -l
-
-sh -c "docker pull mongo"
-sh -c "docker run -d -p 27017-27019:27017-27019 --name mongodb mongo"
---
+++ src/main/java/pl/szczygielski/simplesample/sampletest/SampleTest.java
@@ -0,0 +1,13 @@
+package pl.szczygielski.simplesample.sampletest;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class SampleTest {
+
+    @Test
+    public void sampleTest() {
+        assertEquals(1,1);
+    }
+}
