22ce28970cb82402971c1fd416e7e3ce456145ab
szczygiel9 <szczygielski.daniel38@gmail.com>
2019-10-29 18:34:46 +0100

    New request body for table processing

---
+++ src/main/java/pl/szczygielski/simplesample/service/TableProcessesRequestBody.java
@@ -0,0 +1,15 @@
+package pl.szczygielski.simplesample.service;
+
+import lombok.Getter;
+import pl.szczygielski.simplesample.domain.Table;
+
+@Getter
+public class TableProcessesRequestBody {
+    private Table table;
+    private int rowsToProduce;
+
+    public TableProcessesRequestBody(Table table, int rowsToProduce) {
+        this.table = table;
+        this.rowsToProduce = rowsToProduce;
+    }
+}
