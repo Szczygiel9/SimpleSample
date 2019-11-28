874dcfe93541435d6c4b4b9b30ce7ef77049aaab
szczygiel9 <szczygielski.daniel38@gmail.com>
2019-11-28 20:15:47 +0100

    creating values factories in data types enum

--- src/main/java/pl/szczygielski/simplesample/domain/ColumnType.java
+++ src/main/java/pl/szczygielski/simplesample/domain/ColumnType.java
@@ -1,5 +1,22 @@
 package pl.szczygielski.simplesample.domain;
 
+import pl.szczygielski.simplesample.valuefactory.*;
+
 public enum ColumnType {
-    VARCHAR, BOOL, INTEGER, FLOAT, DATE;
+    //TODO find better way of creating factories here
+    VARCHAR(new StringFactory()),
+    BOOL(new BooleanFactory()),
+    INTEGER(new IntegerFactory()),
+    FLOAT(new FloatFactory()),
+    DATE(new DateFactory());
+
+    private DatatypeValueFactory valueFactory;
+
+    ColumnType(DatatypeValueFactory factory) {
+        this.valueFactory = factory;
+    }
+
+    public String produceValue() {
+        return this.valueFactory.produce();
+    }
 }
