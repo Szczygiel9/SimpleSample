34af8fd5841d9b3d9b459d9d9e550281e7f8d460
szczygiel9 <szczygielski.daniel38@gmail.com>
2019-11-28 20:17:26 +0100

    Remove values populators

--- src/main/java/pl/szczygielski/simplesample/populator/ValuesFactory.java
+++
@@ -1,9 +0,0 @@
-package pl.szczygielski.simplesample.populator;
-
-import pl.szczygielski.simplesample.domain.ColumnType;
-
-public interface ValuesFactory {
-
-    public String create(ColumnType type);
-
-}
